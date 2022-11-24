0f3ea636fa610ce4d990f3c825f403aba7850e02
Salvatore De Rosa <zoomsulweb@gmail.com>
2022-11-24 23:22:29 +1100

    fix boilerplate code

--- src/routes/HomeRoute.ts
+++ src/routes/HomeRoute.ts
@@ -1,7 +1,3 @@
 import { Router, IRouter } from "express";
-import expressAsyncHandler from "express-async-handler";
-import { HomeController } from "../controllers/HomeController";
 
 export const HomeRouter: IRouter = Router();
-
-HomeRouter.get("/", expressAsyncHandler(HomeController));
--- src/services/HomeServices.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const renderHomepage = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.send("<h1>Hello World!</h1>");
-};
