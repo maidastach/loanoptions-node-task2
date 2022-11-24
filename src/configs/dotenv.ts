3beb55d8949c657d306fd8aaeb2c87e32a5b72fd
Salvatore De Rosa <zoomsulweb@gmail.com>
2022-11-24 16:13:28 +1100

    added endpoint in .env file

--- src/configs/dotenv.ts
+++ src/configs/dotenv.ts
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// HARDCODING SOME VALUES BECAUSE .env WILL NOT BE IN THE REPO
 export default {
   PORT: process.env.PORT || 5000,
+  API_ENDPOINT:
+    process.env.API_ENDPOINT || "https://api.publicapis.org/entries",
 };
